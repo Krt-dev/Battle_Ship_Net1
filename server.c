5a83614e47c751ff34e6be7be987e4829a03cc09
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2023-11-12 20:16:53 +0800

    created template

---
+++ server.c
@@ -0,0 +1,24 @@
+// Server side (pseudo-code)
+// Establish a socket, bind, listen, and accept connections
+
+// Game setup
+//initialize_game_board();
+
+// Game loop
+//while (!game_over) {
+    // Send current game state to the client
+    //send_game_state_to_client();
+
+    // Receive shot coordinates from the client
+    //receive_shot_from_client();
+
+    // Update game state based on the shot
+    //update_game_state();
+
+    // Check for game over conditions
+    // if (check_game_over()) {
+    //     // Declare winner and end the game
+    //     send_game_over_message();
+    //     break;
+    // }
+//}
